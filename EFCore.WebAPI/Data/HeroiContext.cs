3237eb4864a21a14ca0e8a5798bf5ca345302626
Camilla Batista <camillabatista95@gmail.com>
2021-03-07 03:08:14 -0300

    First commit EF

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=HeroApp;Trusted_Connection=True;");
+        }
+
+        //Informando que a chave primária da tabela HeroiBatalha é composta
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity => 
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
